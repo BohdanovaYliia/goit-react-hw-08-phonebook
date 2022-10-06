8118bdae06a14acfbc945a9626ae23dc86c147f2
BohdanovaYliia <bogdanovaya1989@gmail.com>
2022-10-06 16:12:54 +0300

    Adds solutions.

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,22 @@ import { Wrap, Title, SecondaryTitle } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   addContact = newContactData => {
